45199abbbbf665a30e225217d58ea35657de8035
Action008 <gyanchandrayadav556@gmail.com>
2024-04-28 17:52:12 +0530

    #

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
         appearance={{
           layout: {
             socialButtonsVariant: "iconButton",
-            logoImageUrl: "/icons/yoom-logo.jpg",
+            logoImageUrl: "/icons/logo.jpg",
           },
           variables: {
             colorText: "#fff",
--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -63,6 +63,7 @@ const MeetingRoom = () => {
           <CallParticipantsList onClose={() => setShowParticipants(false)} />
         </div>
       </div>
+
       {/* video layout and call controls */}
       <div className="fixed bottom-0 flex w-full items-center justify-center gap-5">
         <CallControls onLeave={() => router.push(`/`)} />
