598fac66d1ab683391c31d1cc44e09b145e07377
Action008 <gyanchandrayadav556@gmail.com>
2024-05-09 15:19:56 +0530

    j

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -2,9 +2,11 @@ import MeetingTypeList from '@/components/MeetingTypeList';
 
 const Home = () => {
   const now = new Date();
-  const options = { timeZone: 'Asia/Kolkata', hour: '2-digit', minute: '2-digit', hour12: false };
-  const time = now.toLocaleTimeString('en-IN', options);
-  const date = (new Intl.DateTimeFormat('en-IN', { dateStyle: 'full' })).format(now);
+const options = { timeZone: 'Asia/Kolkata', hour12: false, hour: '2-digit' as const, minute: '2-digit' as const };
+const time = now.toLocaleTimeString('en-IN', options);
+const date = (new Intl.DateTimeFormat('en-IN', { dateStyle: 'full' })).format(now);
+
+  
   
 
 
